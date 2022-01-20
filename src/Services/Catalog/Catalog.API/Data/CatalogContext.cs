2ce927da98f8f9733be0c636ad39274e2de8909c
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-01-20 08:58:44 +0100

    Catalog context added

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration[Constants.MONGO_DB_CONNECTION_STRING]);
+            var database = client.GetDatabase(configuration[Constants.MONGO_DB_DATABASE_NAME]);
+
+            Products = database.GetCollection<Product>(configuration[Constants.MONGO_DB_COLLECTION_NAME]);
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
