7f75baf9703d25f966179beef6ce00fb51fad81f
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-01-22 13:34:03 +0100

    Add constant class to store global variables

---
+++ src/Services/Catalog/Catalog.API/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API
+{
+    public class Constants
+    {
+        public const string MONGO_DB_CONNECTION_STRING = "DatabaseSettings:ConnectionString";
+        public const string MONGO_DB_DATABASE_NAME = "DatabaseSettings:DatabaseName";
+        public const string MONGO_DB_COLLECTION_NAME = "DatabaseSettings:CollectionName";
+    }
+}
