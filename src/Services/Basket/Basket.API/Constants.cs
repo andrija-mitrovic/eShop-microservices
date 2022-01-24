27264b797815959b31345d32c271cd4dd0ef42d3
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-01-24 08:30:10 +0100

    Add class for storing global constants in  Basket.API project

---
+++ src/Services/Basket/Basket.API/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Basket.API
+{
+    public class Constants
+    {
+        public const string REDIS_CONNECTION_STRING = "CacheSettings:ConnectionString";
+        public const string GRPC_DISCOUNT_SETTINGS = "GrpcSettings:DiscountUrl";
+    }
+}
