64b2fd75ec971ab667c8f1139ba2ee36e2e4f4f7
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-02-28 08:29:57 +0100

    Add order repository

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/Repositories/OrderRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Interfaces;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence.Repositories
+{
+    public class OrderRepository : BaseRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            return await _dbContext.Orders.Where(o => o.UserName == userName).ToListAsync();
+        }
+    }
+}
