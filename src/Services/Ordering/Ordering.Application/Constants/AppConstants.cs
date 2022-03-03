5a2345729b327b17fd5c12311efa88e904080246
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-03-03 08:54:54 +0100

    Add class for storing global constants

---
+++ src/Services/Ordering/Ordering.Application/Constants/AppConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Application.Constants
+{
+    public class AppConstants
+    {
+        public const string MSSQL_DATABASE_CONNECTION = "MssqlConnectionString";
+        public const string EMAIL_SETTINGS = "EmailSettings";
+        public const string EVENT_BUS_HOST_ADDRESS = "EventBusSettings:HostAddress";
+    }
+}
