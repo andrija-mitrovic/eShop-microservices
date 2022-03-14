c2a705d3d8b936a3ba8f78591a878e80c41366d5
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-03-14 08:20:21 +0100

    Add entity base configuration

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/Configurations/EntityBaseConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Common;
+
+namespace Ordering.Infrastructure.Persistence.Configurations
+{
+    public class EntityBaseConfiguration : IEntityTypeConfiguration<EntityBase>
+    {
+        public void Configure(EntityTypeBuilder<EntityBase> builder)
+        {
+            builder.Property(e => e.CreatedBy).HasMaxLength(100);
+            builder.Property(o => o.LastModifiedBy).HasMaxLength(100);
+        }
+    }
+}
