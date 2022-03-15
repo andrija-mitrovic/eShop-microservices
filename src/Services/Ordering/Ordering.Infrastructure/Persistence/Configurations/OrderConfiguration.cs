860f3ae2512a4a4f39579eab0b264a7c6c526288
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-03-15 08:10:48 +0100

    Add order configuration

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/Configurations/OrderConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.Property(o => o.UserName).HasMaxLength(50).IsRequired();
+            builder.Property(o => o.TotalPrice).IsRequired();
+
+            // BillingAddress
+            builder.Property(o => o.FirstName).HasMaxLength(50).IsRequired();
+            builder.Property(o => o.LastName).HasMaxLength(50).IsRequired();
+            builder.Property(o => o.EmailAddress).HasMaxLength(50).IsRequired();
+            builder.Property(o => o.AddressLine).HasMaxLength(255).IsRequired();
+            builder.Property(o => o.Country).HasMaxLength(100).IsRequired();
+            builder.Property(o => o.State).HasMaxLength(100).IsRequired();
+            builder.Property(o => o.ZipCode).HasMaxLength(50).IsRequired();
+
+            // Payment
+            builder.Property(o => o.CardName).HasMaxLength(50).IsRequired();
+            builder.Property(o => o.CardNumber).HasMaxLength(50).IsRequired();
+            builder.Property(o => o.Expiration).HasMaxLength(50).IsRequired();
+            builder.Property(o => o.CVV).HasMaxLength(50).IsRequired();
+            builder.Property(o => o.PaymentMethod).IsRequired();
+        }
+    }
+}
