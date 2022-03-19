d0eaa966f1bc21fb630fb7836f75c4d621e5fdc7
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-03-19 08:21:30 +0100

    Add dependecy injection class for Ordering infrastructure

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Constants;
+using Ordering.Application.Interfaces;
+using Ordering.Application.Models;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.Persistence.Repositories;
+using Ordering.Infrastructure.Services;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString(AppConstants.MSSQL_DATABASE_CONNECTION)));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.Configure<EmailSettings>(x => configuration.GetSection(AppConstants.EMAIL_SETTINGS));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
