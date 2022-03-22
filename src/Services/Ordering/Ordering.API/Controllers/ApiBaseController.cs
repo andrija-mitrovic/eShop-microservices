3570fe401ffd4d13294dc82983e9cf8472daa1c9
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-03-22 08:26:02 +0100

    Add base api class

---
+++ src/Services/Ordering/Ordering.API/Controllers/ApiBaseController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public abstract class ApiBaseController : ControllerBase
+    {
+        private ISender _mediator;
+
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetService<ISender>();
+    }
+}
