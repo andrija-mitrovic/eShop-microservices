882dca27969800b2e51138ef5bcbe51b65c7b8f5
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-03-30 08:22:43 +0200

    Add class for storing global constants inside Shopping.Aggregator project

---
+++ src/ApiGateways/Shopping.Aggregator/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace Shopping.Aggregator
+{
+    public class Constants
+    {
+        public const string CATALOG_URL = "ApiSettings:CatalogUrl";
+        public const string BASKET_URL = "ApiSettings:BasketUrl";
+        public const string ORDERING_URL = "ApiSettings:OrderingUrl";
+        public const string CATALOG_REQUEST_URI = "/api/v1/Catalog";
+        public const string BASKET_REQUEST_URI = "/api/v1/Basket";
+        public const string ORDER_REQUEST_URI = "/api/v1/Order";
+    }
+}
