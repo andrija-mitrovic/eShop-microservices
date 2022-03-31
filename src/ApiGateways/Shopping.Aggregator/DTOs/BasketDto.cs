225ec3766a6e4a805fd5125e5cc97037129216a3
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-03-31 08:29:36 +0200

    Add basket dto object

---
+++ src/ApiGateways/Shopping.Aggregator/DTOs/BasketDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Shopping.Aggregator.DTOs
+{
+    public class BasketDto
+    {
+        public string UserName { get; set; }
+        public List<BasketItemDto> Items { get; set; } = new List<BasketItemDto>();
+        public decimal TotalPrice { get; set; }
+    }
+}
