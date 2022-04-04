dc79faf52a31509488a55403aff0059f15470b3a
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-04-04 08:18:40 +0200

    Add shopping dto

---
+++ src/ApiGateways/Shopping.Aggregator/DTOs/ShoppingDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Shopping.Aggregator.DTOs
+{
+    public class ShoppingDto
+    {
+        public string UserName { get; set; }
+        public BasketDto BasketWithProducts { get; set; }
+        public IEnumerable<OrderResponseDto> Orders { get; set; }
+    }
+}
