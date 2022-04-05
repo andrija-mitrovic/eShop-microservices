53fe139dec3b66cc0eccbbdeb7b93ac77deecaf6
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-04-05 08:20:12 +0200

    Add basket service

---
+++ src/ApiGateways/Shopping.Aggregator/Interfaces/IBasketService.cs
@@ -0,0 +1,10 @@
+﻿using Shopping.Aggregator.DTOs;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Interfaces
+{
+    public interface IBasketService
+    {
+        Task<BasketDto> GetBasket(string userName);
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -0,0 +1,26 @@
+﻿using Shopping.Aggregator.DTOs;
+using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Interfaces;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BasketService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<BasketDto> GetBasket(string userName)
+        {
+            var response = await _httpClient.GetAsync($"{Constants.BASKET_REQUEST_URI}/{userName}");
+
+            return await response.ReadContentAs<BasketDto>();
+        }
+    }
+}
