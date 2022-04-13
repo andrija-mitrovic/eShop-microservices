fb289081dceff36874238383ef0ee8df613a624f
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-04-13 08:12:29 +0200

    Add basket service

---
+++ src/WebApps/WebMVC/Interfaces/IBasketService.cs
@@ -0,0 +1,12 @@
+﻿using WebMVC.Models;
+using System.Threading.Tasks;
+
+namespace WebMVC.Interfaces
+{
+    public interface IBasketService
+    {
+        Task<BasketModel> GetBasket(string userName);
+        Task<BasketModel> UpdateBasket(BasketModel model);
+        Task CheckoutBasket(BasketCheckoutModel model);
+    }
+}
---
+++ src/WebApps/WebMVC/Services/BasketService.cs
@@ -0,0 +1,47 @@
+﻿using WebMVC.Extensions;
+using WebMVC.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebMVC.Interfaces;
+
+namespace WebMVC.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await _client.GetAsync($"{Constants.BASKET_REQUEST_URI}/{userName}");
+            return await response.ReadContentAs<BasketModel>();
+        }
+
+        public async Task<BasketModel> UpdateBasket(BasketModel model)
+        {
+            var response = await _client.PostAsJson(Constants.BASKET_REQUEST_URI, model);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.ReadContentAs<BasketModel>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong when calling api.");
+            }
+        }
+
+        public async Task CheckoutBasket(BasketCheckoutModel model)
+        {
+            var response = await _client.PostAsJson($"{Constants.BASKET_REQUEST_URI}/Checkout", model);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Something went wrong when calling api.");
+            }
+        }
+    }
+}
