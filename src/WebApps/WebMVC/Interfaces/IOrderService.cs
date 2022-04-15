38c24effa25ffa995622db58ac428c025a1a99da
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-04-15 08:38:50 +0200

    Add Order service

---
+++ src/WebApps/WebMVC/Interfaces/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using WebMVC.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebMVC.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+}
---
+++ src/WebApps/WebMVC/Services/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using WebMVC.Extensions;
+using WebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebMVC.Interfaces;
+
+namespace WebMVC.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            var response = await _client.GetAsync($"{Constants.ORDER_REQUEST_URI}/{userName}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
+        }
+    }
+}
