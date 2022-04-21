8d714d18b4fb41ccf2f1760db0ab8ace19e469ea
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-04-21 08:32:53 +0200

    Add class for storing global constants

---
+++ src/WebApps/WebMVC/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace WebMVC
+{
+    public class Constants
+    {
+        public const string GATEWAY_ADDRESS = "ApiSettings:GatewayAddress";
+        public const string CATALOG_REQUEST_URI = "/Catalog";
+        public const string BASKET_REQUEST_URI = "/Basket";
+        public const string ORDER_REQUEST_URI = "/Order";
+        public const string JSON_CONTENT_TYPE = "application/json";
+    }
+}
